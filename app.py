b3cd3151cf36066f450c148d126d6ce998eeba5b
SkyeBeepNess <60771619+SkyeBeepNess@users.noreply.github.com>
2021-01-07 15:03:34 +0500

    added bot support

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 import requests
+import telegram
+import logging
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, PicklePersistence
 from bs4 import BeautifulSoup
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 
 
@@ -10,21 +16,54 @@ page = requests.get(URL)
 
 
 soup = BeautifulSoup(page.content, 'lxml')
+def fetch():
+	mypizzas = []
+	myprices = []
 
-mypizzas = []
-myprices = []
+	pizzas = soup.find(class_="sc-814yrq-2 bVRcWG")
+	titles = pizzas.find_all(class_="sc-1x0pa1d-1")
+	prices = pizzas.find_all(class_="money__value")
 
-pizzas = soup.find(class_="sc-814yrq-2 bVRcWG")
-titles = pizzas.find_all(class_="sc-1x0pa1d-1")
-prices = pizzas.find_all(class_="money__value")
 
-for pizza in titles:
-	mypizzas += [pizza.text]
-for keys in prices:
-	myprices += [keys.text]
-zip_iterator = zip(mypizzas, myprices)
-a_dictionary = dict(zip_iterator)
+	for pizza in titles:
+		mypizzas += [pizza.text]
+	for keys in prices:
+		myprices += [keys.text]
+	zip_iterator = zip(mypizzas, myprices)
+	pp_dict = dict(zip_iterator)
 
-print(dict(zip(mypizzas, myprices)))
+	return pp_dict
+
+
+def test(update, context):
+	message = fetch()
+	text = ''
+	for keys in message:
+		text += keys.replace('\n ', '') + ': ' + message[keys] + ' Сум \n'
+	print(text)
+	
+	if update.effective_message.text:
+		context.bot.send_message(update.effective_message.chat_id, 'Цена пицц в dodopizza.uz: \n' +  text)
+
+def main():
+	
+
+	pp = PicklePersistence(filename='file')
+	updater = Updater("1410913727:AAG7K8oh9ToQeZEpINTv1KfVW6Yymel_jz4", persistence=pp, use_context=True)
+	dispatcher = updater.dispatcher
+
+	test_handler = MessageHandler(Filters.regex('test'), test)
+	dispatcher.add_handler(test_handler)	
+	
+
+	
+
+
+	updater.start_polling()
+	updater.idle()
+
+
+if __name__ == '__main__':
+    main()
 #for key in prices:
 #	print(key.text)
